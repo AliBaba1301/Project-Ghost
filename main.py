8b15774405c094d86d6f95accb73d211080e2fe4
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2022-03-04 21:22:24 +0000

    Added in video output as well as testing for basic functions such as take off land and rotate.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 from djitellopy import Tello
 import cv2
 
-image_h = 240
-image_w = 320
+image_h = 600
+image_w = 800
 flight_mode = 1 #0 to turn motors on 1 for testing
 
 # Connecting to the Tello Drone
@@ -28,13 +28,29 @@ def stream_control(drone,command):
     else:
         drone.streamoff()
 
-
-
-
 def main():
     drone =  connect_to_drone()
-    print(drone.get_battery)
+    print(drone.get_battery())
     stream_control(drone,'on')
+
+    while True:
+        frames = drone.get_frame_read()
+        frame = frames.frame
+        vid_stream = cv2.resize(frame,(image_w,image_h))
+
+        if flight_mode == 0:
+            drone.takeoff()
+            drone.rotate_counter_clockwise(90)
+            drone.rotate_clockwise(90)
+
+        cv2.imshow('window',vid_stream)
+
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            stream_control(drone,'off')
+            drone.land()
+            break
+
+
 
 
 
