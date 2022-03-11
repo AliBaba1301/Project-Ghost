794a0e49c5edc01e7c25314e0c671c8be6ca8891
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2022-03-11 19:38:56 +0000

    Test

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# import pandas as pd
-# import numpy as np
-# import keras
-# import tensorflow as tf
 from djitellopy import Tello
 import cv2
+import numpy
+import time
+
 
 image_h = 600
 image_w = 800
-flight_mode = 1 #0 to turn motors on 1 for testing
+flight_mode = 0 #0 to turn motors on 1 for testing
 
 # Connecting to the Tello Drone
 def connect_to_drone():
     drone = Tello()
     drone.connect()
-    drone.for_back_velocity = 0
+    drone.for_back_velocity = 0  
     drone.left_right_velocity = 0
     drone.up_down_velocity = 0
     drone.yaw_velocity = 0
@@ -37,21 +36,24 @@ def main():
         frames = drone.get_frame_read()
         frame = frames.frame
         vid_stream = cv2.resize(frame,(image_w,image_h))
+          
 
         if flight_mode == 0:
             drone.takeoff()
             drone.rotate_counter_clockwise(90)
             drone.rotate_clockwise(90)
-
+            drone.move_left(35)
+            drone.move_right(35)
+        
         cv2.imshow('window',vid_stream)
 
-        if cv2.waitKey(1) & 0xFF == ord('a'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             stream_control(drone,'off')
             drone.land()
             break
 
 
-
+   
 
 
 
