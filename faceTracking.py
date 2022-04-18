4bdf09c2c37e89adb2e93eda513feb04c5771b4b
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2022-04-18 18:48:23 +0100

    Testing to add in Z axis movement to the tracking

--- faceTracking.py
+++ faceTracking.py
@@ -17,6 +17,7 @@ def findFace(img):
 
     for(x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 115, 126), 2)
+        cv2.circle(img, (x + w//2,y+ h//2),2,(255, 115, 126),0)
 
         # Ensures that the closest face is the one being followed
         cx = x + w/2
@@ -28,26 +29,37 @@ def findFace(img):
         if len(faceAreaList) != 0:
             i = faceAreaList.index(max(faceAreaList))
             return img, [faceCentreList[i], faceAreaList[i]]
-        
+
     return img, [[0, 0], 0]
 
 
-def trackFace(drone, info, w,h, pid, p_error_w, p_error_h):
+def trackFace(drone, info, w, h, pid, p_error_w, p_error_h, p_error_area):
 
     # PID Controller - Used for smoothing drone transitions
     optimal_w = w//2
     error_w = info[0][0] - optimal_w
     speed_w = pid[0] * error_w + pid[1] * (error_w - p_error_w)
-    speed_w = int(np.clip(speed_w, -100, 100)) # keeps the speed withins range of drone
+    # keeps the speed withins range of drone
+    speed_w = int(np.clip(speed_w, -100, 100))
 
     optimal_h = h//2
     error_h = info[0][1] - optimal_h
     speed_h = pid[0] * error_h + pid[1] * (error_h - p_error_h)
-    speed_h = int(np.clip(speed_h, -100, 100)) # keeps the speed withins range of drone
+    # keeps the speed withins range of drone
+    speed_h = int(np.clip(speed_h, -100, 100))
+
+    optimal_area = 5500
+    error_area = info[1] - optimal_area
+    speed_area = pid[0] * error_area + pid[1] * (error_area - p_error_area)
+    # keeps the speed withins range of drone
+    speed_area = int(np.clip(speed_area, -100, 100))
+
+
 
     if info[0][0] != 0 and info[0][1] != 0:
         drone.yaw_velocity = speed_w
-        drone.up_down_velocity = speed_h
+        drone.up_down_velocity = (speed_h*-1)
+        drone.for_back_velocity = (speed_area*-1)
     else:
         drone.for_back_velocity = 0
         drone.left_right_velocity = 0
@@ -60,4 +72,4 @@ def trackFace(drone, info, w,h, pid, p_error_w, p_error_h):
         drone.send_rc_control(drone.left_right_velocity, drone.for_back_velocity,
                               drone.up_down_velocity, drone.yaw_velocity)
 
-    return error_w, error_h
+    return error_w, error_h, error_area
--- main.py
+++ main.py
@@ -4,61 +4,61 @@ import numpy as np
 import time
 from faceTracking import *
 
-flight_mode = 0 #0 to turn motors on 1 for testing
+flight_mode = 0  # 0 to turn motors on 1 for testing
 image_h = 360
 image_w = 480
-pid = [0.5,0,5,0]
-p_error_w,p_error_h = 0,0
-
+pid = [0.5, 0.5, 0]
+p_error_w, p_error_h, p_error_area = 0, 0, 0
 
 
 # Connecting to the Tello Drone
 def connect_to_drone():
     drone = Tello()
     drone.connect()
-    drone.for_back_velocity = 0  
+    drone.for_back_velocity = 0
     drone.left_right_velocity = 0
     drone.up_down_velocity = 0
     drone.yaw_velocity = 0
-    drone.speed = 0 
+    drone.speed = 0
     print('connected')
     return(drone)
 
 # Method to turn stream on or off
-def stream_control(drone,command):
+
+
+def stream_control(drone, command):
     if command == 'on':
         drone.streamon()
     else:
         drone.streamoff()
 
+
 def main():
-    global flight_mode , p_error_w, p_error_h
-    drone =  connect_to_drone()
+    global flight_mode, p_error_w, p_error_h, p_error_area
+    drone = connect_to_drone()
     print(drone.get_battery())
-    stream_control(drone,'on')
-
-
+    stream_control(drone, 'on')
 
     while True:
-        
+
         # Fly
         if flight_mode == 0:
             drone.takeoff()
             flight_mode = 1
 
         frames = drone.get_frame_read()
-        frame = cv2.resize(frames.frame,(image_w,image_h))
-        vid_stream , info = findFace(frame)
-        cv2.imshow('window',vid_stream)  
+        frame = cv2.resize(frames.frame, (image_w, image_h))
+        vid_stream, info = findFace(frame)
+        output_concat = np.concatenate((frame, vid_stream), axis=1) 
 
-        p_error_w, p_error_h = trackFace(drone, info, image_w,image_h,pid,p_error_w, p_error_h)
-        # # print(info[0][0])
-        # cv2.imshow('window',vid_stream)  
+        cv2.imshow('window', output_concat)
+
+        p_error_w, p_error_h , p_error_area= trackFace(
+            drone, info, image_w, image_h, pid, p_error_w, p_error_h, p_error_area)
         
-        
 
 
-        # Testing to see if drone can perform basic commands when assessing for damage. 
+        # Testing to see if drone can perform basic commands when assessing for damage.
         # if flight_mode == 0:
         #     drone.takeoff()
         #     drone.rotate_counter_clockwise(90)
@@ -69,16 +69,12 @@ def main():
         #     drone.move_down(35)
         #     drone.move_forward(35)
         #     drone.move_back(35)
-        
-       
 
-        if cv2.waitKey(1) and 0xFF == ord('q'):
+        print(drone.get_battery())
+
+        if cv2.waitKey(1) and 0xFF == ord('a'):
             drone.land()
-            stream_control(drone,'off')
-
-
-   
-
+            stream_control(drone, 'off')
 
 
 if __name__ == '__main__':
